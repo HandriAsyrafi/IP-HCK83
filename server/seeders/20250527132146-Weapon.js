4f5f7797e53cddc9ba8a29b03b53729bdc09a164
Handriza Asyrafi <handriza.asyrafi@gmail.com>
2025-05-27 23:56:36 +0700

    setup seeding weapons

--- server/seeders/20250527132146-Weapon.js
+++ server/seeders/20250527132146-Weapon.js
@@ -1,25 +1,34 @@
-'use strict';
+"use strict";
+
+const axios = require("axios");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    const response = await axios.get("https://wilds.mhdb.io/en/weapons");
+
+    const allWeapons = response.data;
+
+    const highRarityWeapons = allWeapons
+      .filter((weapon) => weapon.rarity >= 7)
+      .map((weapon) => ({
+        name: weapon.name,
+        kind: weapon.kind,
+        rarity: weapon.rarity,
+        damage: weapon.damage.raw,
+        element: weapon.specials.map((el) => el.element),
+        elementDamage: weapon.specials.map((el) => el.damage.display),
+      }));
+
+    console.log(highRarityWeapons);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
