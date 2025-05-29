8cb8993963e6fdbd91f048e83cf182ac156b9b4c
Handriza Asyrafi <handriza.asyrafi@gmail.com>
2025-05-29 17:06:56 +0700

    fix app.jsx

--- client/ip-client/src/App.jsx
+++ client/ip-client/src/App.jsx
@@ -9,8 +9,8 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
